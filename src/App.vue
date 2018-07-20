306331f8add74e6dd6c32ab1d5134dc0c1c47ffe
Daniel Sanchez <daniel.sanchez@apscreativas.com>
2018-07-20 09:34:13 -0500

    Borrando viejo menu y agregando nuevo

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,10 @@
     <f7-panel left reveal>
           <f7-block-title>Menu</f7-block-title>
           <f7-list>
-            <f7-list-item title="Inicio" link="/inicio"></f7-list-item>
-            <f7-list-item title="Formulario" link="/formulario"></f7-list-item>
-            <f7-list-item title="Mapa" link="/mapa"></f7-list-item>
-            <f7-list-item title="Salir" link="/"></f7-list-item>
-          </f7-list>
-          <f7-list simple-list>
-            <f7-list-item>
-              <f7-link href="/inicio" panel-close>Dashboard</f7-link>
-            </f7-list-item>
-            <f7-list-item>
-              <f7-link href="/" panel-close>Mapa</f7-link>
-            </f7-list-item>
-            <f7-list-item>
-              <f7-link href="/formulario" panel-close>Formulario</f7-link>
-            </f7-list-item>
-            <f7-list-item>
-              <f7-link href="/" panel-close>Salir</f7-link>
-            </f7-list-item>
+            <f7-list-item title="Inicio" link="/inicio" panel-close></f7-list-item>
+            <f7-list-item title="Formulario" link="/formulario" panel-close></f7-list-item>
+            <f7-list-item title="Mapa" link="/mapa" panel-close></f7-list-item>
+            <f7-list-item title="Salir" link="/" panel-close></f7-list-item>
           </f7-list>
     </f7-panel>
 
@@ -40,13 +26,6 @@
 export default {
   name: 'App',
   methods: {
-    algo(){
-      console.log("algo");
-      console.log(this.$f7.router.navigate('/'));
-      this.$f7.router.navigate('/');
-      //this.$f7router.navigate('/')
-    },
-
     handleBackButton() {
       // NOTE: The back button will behave differently depending on circumstance
       // If the side panel is open, close it
