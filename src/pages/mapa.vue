cad9952c8292d4280630500dac2f14c7a18f03b1
Daniel Sanchez <conejodan@gmail.com>
2018-08-01 23:26:52 -0500

    Agregando funcionalidades al mapa

--- src/pages/mapa.vue
+++ src/pages/mapa.vue
@@ -10,11 +10,13 @@
         </f7-nav-left>
         <f7-nav-title>Mapa</f7-nav-title>
         <f7-nav-right>
-            
+                            <f7-button @click="get_map = !get_map" fill :color="(get_map)?'green':'red'" >
+<f7-icon f7="compass_fill" ></f7-icon>
+              </f7-button>
         </f7-nav-right>
     </f7-navbar>
     <f7-block v-if="ubication">
-      <GmapMap
+      <GmapMap v-if="get_map"
                         ref="googleMap"
                         :center="mapa.center_map"
                         :zoom="mapa.zoom_map"
@@ -47,14 +49,43 @@
               <f7-list-item :title="'Area: ' + calculatArea"></f7-list-item>
             </template>
           </f7-list>
-          <f7-block inner v-if="puntos.length>=3">
+          <f7-block inner v-if="puntos.length>0">
             <f7-row>
               <f7-col>
-                <f7-button fill @click="borrar" color="red">Borrar Puntos</f7-button>
+                <f7-button fill @click="borrar_ultimo_punto" color="red">Borrar ultimo Punto</f7-button>
               </f7-col>
             </f7-row>
           </f7-block>
     </f7-block>
+    <f7-fab position="right-bottom" slot="fixed" v-if="puntos.length>=3">
+      <f7-icon f7="add" md="material:add"></f7-icon>
+      <f7-icon f7="arrow_down" md="material:close"></f7-icon>
+      <f7-fab-buttons position="top">
+        <f7-fab-button label="Ver puntos" fab-close color="orange" @click="popupOpened = true">
+          <f7-icon f7="eye" md="material:close"></f7-icon>
+        </f7-fab-button>
+        <f7-fab-button label="Guardar Puntos" fab-close color="green">
+          <f7-icon f7="document_text_fill"></f7-icon>
+        </f7-fab-button>
+        <f7-fab-button label="Eliminar Puntos" fab-close color="red" @click="borrar">
+          <f7-icon f7="close"></f7-icon>
+        </f7-fab-button>
+      </f7-fab-buttons>
+    </f7-fab>
+    <f7-popup class="demo-popup" :opened="popupOpened" @popup:closed="popupOpened = false">
+      <f7-page>
+        <f7-navbar title="Detalle de Puntos">
+          <f7-nav-right>
+            <f7-link popup-close>Cerrar</f7-link>
+          </f7-nav-right>
+        </f7-navbar>
+        <f7-block>
+          <f7-list simple-list>
+              <f7-list-item v-for="(punto,index) in puntos_detalle" :key="index" :title="punto"></f7-list-item>
+          </f7-list>
+        </f7-block>
+      </f7-page>
+    </f7-popup>
   </f7-page>
 </template>
 <script>
@@ -66,11 +97,16 @@ export default {
     return {
       height:window.innerHeight / 3,
       db:null,
+      get_map:true,
       mapa:{
           center_map:{lat:24.083304, lng:-102.339398},
           zoom_map:5
       },
       ubication:false,
+      popupOpened:false,
+      last_punto:"",
+      puntos_firebase:null,
+      puntos_detalle:[],
       puntos:[
         // {lat:18.0361701, lng:-92.9304299},
         // {lat:17.9851545, lng:-92.9072556},
@@ -114,13 +150,25 @@ export default {
       
     },
     cargarPuntos(puntos){
-      console.log("CargandoPuntos->", puntos)
+      console.log("CargandoPuntos->", puntos);
+      this.puntos_firebase = puntos;
+      this.puntos = [];
+      this.puntos_detalle = [];
       if(puntos){
-        this.puntos = [];
         console.log("Tiene puntos")
         for(let key in puntos){
           let {lat, lng} = puntos[key];
           this.puntos.push({lat,lng});
+        }
+        if(this.puntos.length>1){
+          for(let i = 0; i< this.puntos.length-1;i++){
+            let distancia = this.getDistanceBetween(this.puntos[i],this.puntos[i+1]).toFixed(2)
+            this.puntos_detalle.push("Trazo " + (i+1) + ": " + distancia + " metros")
+            if(this.puntos.length>2 && ((i+1) == this.puntos.length)){
+              let distancia = this.getDistanceBetween(this.puntos[i+1],this.puntos[0])
+              this.puntos_detalle.push("Trazo " + (i+2) + ": " + distancia + " metros")
+            }
+          }
         }
       }
       //this.puntos = puntos;
@@ -129,6 +177,16 @@ export default {
       this.puntos = [];
       let usuario = firebase.auth().currentUser;
       this.db.ref("/"+ usuario.uid + "/puntos").remove().then(()=>{
+              console.log("Enviado")
+          })
+    },
+    borrar_ultimo_punto(){
+      let last_key= "";
+      for(let key in this.puntos_firebase){
+          last_key = key;
+        }
+      let usuario = firebase.auth().currentUser;
+      this.db.ref("/"+ usuario.uid + "/puntos/"+ last_key).remove().then(()=>{
               console.log("Enviado")
           })
     },
